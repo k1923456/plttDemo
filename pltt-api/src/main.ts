28667a3e5e22125d3cd523dcdc8398cf693df5a2
santai-wang-7senses <88714960+santai-wang-7senses@users.noreply.github.com>
2021-12-03 07:50:41 +0800

    feat: make port 3210

--- pltt-api/src/main.ts
+++ pltt-api/src/main.ts
@@ -6,7 +6,7 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 
   if (module.hot) {
     module.hot.accept();
